23397b013443dbd8b1549291a650130f353c61ff
TonO <tonot1@hotmail.com>
2023-01-28 19:44:10 +0100

    Revert "Update VideoInfoScanner.cpp"
    This reverts commit 9b3078a825e8de6594adcf55ebcca17f44b410f6.

--- xbmc/video/VideoInfoScanner.cpp
+++ xbmc/video/VideoInfoScanner.cpp
@@ -183,11 +183,6 @@ namespace VIDEO
     m_pathsToScan.clear();
     m_pathsToClean.clear();
 
-CLog::Log(
-          LOGINFO,
-          "VideoInfoScanner: Start in directory '{}', content type '{}'",
-          CURL::GetRedacted(strDirectory), TranslateContent(content));
-
     m_database.Open();
     if (strDirectory.empty())
     { // scan all paths in the database.  We do this by scanning all paths in the db, and crossing them off the list as
@@ -205,19 +200,10 @@ CLog::Log(
       for (std::vector<std::string>::const_iterator it = rootDirs.begin(); it < rootDirs.end(); ++it)
       {
         m_pathsToScan.insert(*it);
-CLog::Log(
-          LOGINFO,
-          "VideoInfoScanner: Start dir '{}'",
-          CURL::GetRedacted(*it));
-
         std::vector<std::pair<int, std::string>> subpaths;
         m_database.GetSubPaths(*it, subpaths);
         for (std::vector<std::pair<int, std::string>>::iterator it = subpaths.begin(); it < subpaths.end(); ++it)
           m_pathsToScan.insert(it->second);
-          CLog::Log(
-          LOGINFO,
-          "VideoInfoScanner: Sub dir '{}'",
-          CURL::GetRedacted(it->second));
       }
     }
     m_database.Close();
@@ -275,7 +261,7 @@ CLog::Log(
       return true;
 
     if (HasNoMedia(strDirectory))
-      return true;  
+      return true;
 
     bool ignoreFolder = !m_scanAll && settings.noupdate;
     if (content == CONTENT_NONE || ignoreFolder)
